0b729d4d7d0f7dbc74f7439874a760de78bd20ea
kumar <kvenkat2006@yahoo.com>
2018-01-15 14:42:32 -0500

    Jan 15

--- src/main/scala/DataAggrProto.scala
+++ src/main/scala/DataAggrProto.scala
@@ -30,36 +30,15 @@ object DataAggrProto {
   .option("subscribe", "test")
   .option("startingOffsets", "earliest")
   .load()
-  //.selectExpr("CAST(key AS STRING)","CAST(value AS STRING)")
   .selectExpr("CAST(value AS STRING)")
-  //.as[(String, String)]
   .as[String]
 
-  // Generate running word count
- // val wordCounts = lines.flatMap(_.split(" ")).groupBy("value").count()
-  //val wordCounts = lines.map(_.split(",")).filter(attr => attr(4) == 1).map(attr => (attr(3), attr(5)) ).groupByKey
-//.groupBy("value").count()
-
-//case class DealPnlData(busDate: String, dealId: String, prodId: String, portFolioId: String, scenarioId: Int, pnl: Float)
-
-   val wordCounts = lines.map(_.split(","))
+   val rawDataDF = lines.map(_.split(","))
                           .map(attr => DealPnlData(attr(0), attr(1),attr(2),attr(3),attr(4).trim.toInt,attr(5).trim.toFloat ) ).toDF()
 
-    //val ds: Dataset[DealPnlData] = wordCounts.as[DealPnlData]
 
-    wordCounts.createOrReplaceTempView("pnlStructTable")
+    rawDataDF.createOrReplaceTempView("pnlStructTable")
     val aggDf = spark.sql("select portfolioId, sum(pnl) from pnlStructTable group by portfolioId")
-    // Start running the query that prints the running counts to the console
-  //val query = wordCounts.writeStream
-  //.outputMode("complete")
-  //.format("console")
-  //.start()
-
-  //val wordCounts = lines.groupBy("key").count()
-//  val query = wordCounts.writeStream
-//  .outputMode("complete")
-//  .format("console")
-//  .start()
 
     val query = aggDf.writeStream.outputMode("complete").format("console").start()
 
