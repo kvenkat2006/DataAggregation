dea22ce0051da5e282643d33453aab478edf24cb
Ranjini TR <35687802+RanjiniTR@users.noreply.github.com>
2018-02-11 17:27:59 +0530

    Typesafe Changes on Feb 11

--- src/main/scala/StaticDataAggrProto.scala
+++ src/main/scala/StaticDataAggrProto.scala
@@ -1,26 +1,35 @@
 package com.dhee
+
 //import org.apache.spark.sql.expressions.Aggregator
 //import org.apache.spark.sql.Encoder
 //import org.apache.spark.sql.Encoders
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
+
 //RanSud: Typesafe changes next 1 line
 import com.typesafe.config.ConfigFactory
+
 object StaticDataAggrProto {
   def main(args: Array[String]): Unit = {
-//RanSud:Typesafe changes next 1 line
+
+//RanSud: Typesafe changes next 6 line
     val config = ConfigFactory.parseFile(new java.io.File("src/main/scala/dataagg.conf"))
-    val url = "jdbc:postgresql://localhost:5432/DATA_AGGR"
+
+    val db = config.getString("dataagg.config.consumer.dbname")
+
+//  val url = "jdbc:postgresql://localhost:5432/DATA_AGGR"
+    val url = s"""jdbc:postgresql://localhost:5432/$db"""
+
     val prop = new java.util.Properties
-//Sudha: Typesafe change 2 lines
-    val usr = config.getString("dataagg.config.general.pgresuser")
-    val pwd = config.getString("dataagg.config.general.pgrespw")
     prop.setProperty("driver", "org.postgresql.Driver")
-//Sudha:Typesafe change 4 lines
-    prop.setProperty("user", usr)
+
+//RanSud: Typesafe changes next 5 lines
+    val user = config.getString("dataagg.config.general.pgresuser")
+    val pwd = config.getString("dataagg.config.general.pgrespw")
+
+    prop.setProperty("user", user)
     prop.setProperty("password", pwd)
-    //prop.setProperty("user", "developer")
-    //prop.setProperty("password", "developer")
+
     val spark = SparkSession
       .builder()
       .master("local[2]")
@@ -28,52 +37,54 @@ object StaticDataAggrProto {
       //.config("spark.some.config.option", "xxx")
       .getOrCreate()
 
-//Sudha: Typesafe change 3 line
-        val basedfpath = config.getString("dataagg.config.consumer.baseDFloc")
-        val rawDataDF = spark.read.load(s"""$basedfpath/*.parquet""")
-      // val rawDataDF = spark.read.load("/home/kumar/DataAggregation/baseDfData/*.parquet")
-   rawDataDF.createOrReplaceTempView("pnlStructTable")
+//RanSud: Typesafe changes next 3 lines
+    val baseDFpath = config.getString("dataagg.config.consumer.baseDFloc")
+//  val rawDataDF = spark.read.load("/home/kumar/DataAggregation/baseDfData/*.parquet")
+    val rawDataDF = spark.read.load(s"""$baseDFpath/*.parquet""")
+
+    rawDataDF.createOrReplaceTempView("pnlStructTable")
     rawDataDF.printSchema()
+
     println("\n\n ******* Count of elements in rawDataDF : " + rawDataDF.count())
+
     val baseAggDf = spark.sql(
       s"""select portfolioId as portfolio_id, scenarioId as scenario_id, sum(pnl) as sum_pnl
          |from pnlStructTable WHERE baseOrWhatif = 'BASE' group by portfolioId, scenarioId""".stripMargin)
+
     val rawBaseSubsetDF = spark.sql(
       s"""select busDate, dealId, prodId, portFolioId, scenarioId, pnl, baseOrWhatIf, uid, eventTime
          | from pnlStructTable WHERE baseOrWhatif = 'BASE' """.stripMargin)
-    println("\n\n ******* Count of elements in rawBaseSubsetDF : " + rawBaseSubsetDF.count())
-    val rawWhatIfSubsetDF = spark.sql(
 
-    s"""select busDate, dealId, prodId, portFolioId, scenarioId, pnl as whatIfPnl, baseOrWhatIf, uid, eventTime
+    println("\n\n ******* Count of elements in rawBaseSubsetDF : " + rawBaseSubsetDF.count())
+
+    val rawWhatIfSubsetDF = spark.sql(
+      s"""select busDate, dealId, prodId, portFolioId, scenarioId, pnl as whatIfPnl, baseOrWhatIf, uid, eventTime
          | from pnlStructTable WHERE baseOrWhatif = 'WHATIF' """.stripMargin)
+
     println("\n\n ******* Count of elements in rawWhatIfSubsetDF : " + rawWhatIfSubsetDF.count())
-//    val testDF = rawBaseSubsetDF.join(rawWhatIfSubsetDF,
-//                    Seq("busDate", "dealId", "prodId", "portFolioId", "scenarioId"),
-//                    "inner")
-//
-//    testDF.show()
+
     val rawBaseJoinWhatIfDF = rawBaseSubsetDF.join(rawWhatIfSubsetDF,
                                             Seq("busDate", "dealId", "prodId", "portFolioId", "scenarioId"),
                                                 "left_outer")
+
     println("\n\n ******* Count of elements in rawBaseJoinWhatIfDF : " + rawBaseJoinWhatIfDF.count())
     //rawBaseJoinWhatIfDF
+
     val whatIfFusedDF = rawBaseJoinWhatIfDF.select(rawBaseSubsetDF("busDate"),
                           rawBaseSubsetDF("dealId"),
                           rawBaseSubsetDF("prodId"),
-rawBaseSubsetDF("portFolioId"),
+                          rawBaseSubsetDF("portFolioId"),
                           rawBaseSubsetDF("scenarioId"),
                           when(rawWhatIfSubsetDF("whatIfPnl").isNotNull,rawWhatIfSubsetDF("whatIfPnl"))
                                     .otherwise(rawBaseSubsetDF("pnl")).as("pnl")
                           )
+
     whatIfFusedDF.createOrReplaceTempView("whatIfFusedTable")
     val whatIfAggDf = spark.sql(
       s"""select portfolioId as portfolio_id, scenarioId as scenario_id, sum(pnl) as sum_pnl
          |from whatIfFusedTable group by portfolioId, scenarioId""".stripMargin)
+
     whatIfAggDf.write.mode("overwrite").jdbc(url, "whatif_aggr", prop)
-//    while(true) {
-//      println("HELLO ")
-//    }
+
   }
 }
-
-
